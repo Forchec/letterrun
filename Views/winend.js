87b275873d9713c679400bfe792a73b70709661a
forchec <connerforche@gmail.com>
2019-05-02 15:44:46 -0700

    Added Time to Win screen on all stages, Changed Position of Time so that 3 digit numbers wont touch play button

--- Views/winend.js
+++ Views/winend.js
@@ -2,7 +2,7 @@ document.write('\
 \
 <div id="WinGame" class="modal">\
   <div class="modal-content grid-container">\
-    <div class="header"><p><center>Congratulations!</center></p></div>\
+    <div id="winText" class="header"><p><center>Congratulations!</center></p></div>\
     <button id="menu_button_1" class="menu_button">Menu</button>\
     <button id="replay_button_1" class="replaybutton">Replay</button>\
   </div>\
