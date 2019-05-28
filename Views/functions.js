b5edb9ebbb8c2c508eb74ebb4d04a4270c3b4f13
forchec <forche@spu.edu>
2019-05-27 20:25:49 -0700

    Changed location of some assets that were previously in view folder, Removed duplicate code from stages so that function.js can take care of it

--- Views/functions.js
+++ Views/functions.js
@@ -30,6 +30,9 @@ function preload() {
   this.load.image('sky', '../newAssets/background/Background.png');
   this.load.image('ground', '../newAssets/background/Grass.png');
   this.load.audio('song', '../newAssets/bensound-endlessmotion.mp3');
+  for (var i = 1; i < 18; i++) {
+    this.load.image('water' + i, '../newAssets/Platform Game Assets/Water Animation/png/1x/image ' + (i) + '.png');
+  }
   var color = localStorage.getItem("color");
   switch (color) {
     case "blue":
